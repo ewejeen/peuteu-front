4ef4ca2dc62bd2bfb4a19820b6ff373ff8b4de36
Yoojin Kim <yoojinkim24@gmail.com>
2024-10-31 16:11:27 +0900

    🔨 [CHORE] 빌드 환경 분리 및 빌드 스크립트 수정

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,14 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
 
   pluginOptions: {
     vuetify: {
-			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
-		}
-  }
-})
+      // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
+    },
+  },
+
+  devServer: {
+    port: process.env.VUE_APP_PORT || 3131,
+  },
+});
